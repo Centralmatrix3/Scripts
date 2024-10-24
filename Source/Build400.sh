ce8ae0b83a28786d6e69b61449583d80e0808ecf
Matrix <152264868+Centralmatrix3@users.noreply.github.com>
2024-10-24 22:57:28 +0800

    Build

--- Source/Build400.sh
+++ Source/Build400.sh
@@ -1,1 +1,40 @@
 #!/bin/bash
+
+# 创建规则目录: Matrix-io
+mkdir -p Matrix-io/{Clash,Egern,Loon,QuantumultX,Shadowrocket,Sing-box,Stash,Surge}/Ruleset
+
+# 规则名称: ABC
+for dir in Clash Egern Loon QuantumultX Shadowrocket Sing-box Stash Surge; do
+    if [[ $dir == "Clash" || $dir == "Egern" || $dir == "Stash" ]]; then
+        file_ext="ABC.yaml"
+    elif [[ $dir == "Sing-box" ]]; then
+        file_ext="ABC.json"
+    else
+        file_ext="ABC.list"
+    fi
+    curl -L -o Matrix-io/$dir/Ruleset/$file_ext "https://raw.githubusercontent.com/Centralmatrix3/Scripts/master/Ruleset/ABC.list"
+done
+
+# 规则名称: BBC
+for dir in Clash Egern Loon QuantumultX Shadowrocket Sing-box Stash Surge; do
+    if [[ $dir == "Clash" || $dir == "Egern" || $dir == "Stash" ]]; then
+        file_ext="BBC.yaml"
+    elif [[ $dir == "Sing-box" ]]; then
+        file_ext="BBC.json"
+    else
+        file_ext="BBC.list"
+    fi
+    curl -L -o Matrix-io/$dir/Ruleset/$file_ext "https://raw.githubusercontent.com/Centralmatrix3/Scripts/master/Ruleset/BBC.list"
+done
+
+# 规则名称: CCB
+for dir in Clash Egern Loon QuantumultX Shadowrocket Sing-box Stash Surge; do
+    if [[ $dir == "Clash" || $dir == "Egern" || $dir == "Stash" ]]; then
+        file_ext="CCB.yaml"
+    elif [[ $dir == "Sing-box" ]]; then
+        file_ext="CCB.json"
+    else
+        file_ext="CCB.list"
+    fi
+    curl -L -o Matrix-io/$dir/Ruleset/$file_ext "https://raw.githubusercontent.com/Centralmatrix3/Scripts/master/Ruleset/CCB.list"
+done
