78a8511937999d9db0f790d4b92a310d4f82caa2
Matrix <152264868+Centralmatrix3@users.noreply.github.com>
2025-05-14 22:20:24 +0800

    更新 QuantumultX.js

--- Scripts/Resource-Parser/QuantumultX.js
+++ Scripts/Resource-Parser/QuantumultX.js
@@ -8,15 +8,6 @@ resource_parser_url = https://github.com/Centralmatrix3/Scripts/raw/master/Scrip
 ----------------------------------------
 */
 
-/**
-* 使用说明，
-0️⃣ 在QuantumultX 配置文件中[general] 部分，加入 
-resource_parser_url = https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/resource-parser.js
-⚠️⚠️如提示"没有自定义解析器"，请长按右下角图标后点击左侧刷新按钮，更新资源，后台退出 app，直到出现解析器说明
-
-------------------------------
-*/
-
 //beginning 解析器正常使用，調試註釋此部分
 
 let [link0, content0, subinfo] = [$resource.link, $resource.content, $resource.info]
