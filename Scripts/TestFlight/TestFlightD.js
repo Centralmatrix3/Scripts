eea3f6c25f59c59c0159fa0eec22cf4061ef5bcf
Matrix <152264868+Centralmatrix3@users.noreply.github.com>
2025-11-26 01:18:30 +0800

    2025-11-26

--- Scripts/TestFlight/TestFlightD.js
+++
@@ -1,9 +0,0 @@
-/*
- * @name = TestFlightD
- * @desc = TestFlightD
- * @author = NobyDa
-*/
-
-let app = JSON.parse($request.body);
-app.storefrontId = '143441-19,29';
-$done({body:JSON.stringify(app)});
